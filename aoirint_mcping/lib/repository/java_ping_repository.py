cc79ed728f926e2efe16263974916df5fc9bef60
aoirint <aoirint@gmail.com>
2023-02-19 10:10:49 +0900

    java ping repository

---
+++ aoirint_mcping/lib/repository/java_ping_repository.py
@@ -0,0 +1,54 @@
+import asyncio
+from abc import ABC, abstractmethod
+
+from mcstatus import JavaServer
+from pydantic import BaseModel
+
+
+class JavaPingTimeoutError(Exception):
+    pass
+
+
+class JavaPingResultPlayer(BaseModel):
+    id: str
+    name: str
+
+
+class JavaPingResult(BaseModel):
+    version_protocol: int
+    version_name: str
+    latency: float
+    players_online: int
+    players_max: int
+    players_sample: list[JavaPingResultPlayer]
+    description: str
+    favicon: str | None # Data URL
+
+
+class JavaPingRepository(ABC):
+    @abstractmethod
+    def ping(self, host: str, port: int, timeout: float) -> JavaPingResult | None:
+        ...
+
+
+class JavaPingRepositoryImpl(JavaPingRepository):
+    def ping(self, host: str, port: int, timeout: float) -> JavaPingResult | None:
+        try:
+            server = JavaServer.lookup(address=f"{host}:{port}", timeout=timeout)
+            response = server.status()
+
+            # sample is None when no player logged in
+            players_sample = response.players.sample if response.players.sample is not None else []
+
+            return JavaPingResult(
+                version_protocol=response.version.protocol,
+                version_name=response.version.name,
+                latency=response.latency,
+                players_online=response.players.online,
+                players_max=response.players.max,
+                players_sample=players_sample,
+                description=response.description,
+                favicon=response.favicon,
+            )
+        except asyncio.TimeoutError:
+            raise JavaPingTimeoutError
